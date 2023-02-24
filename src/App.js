832b06f92b364d2c9792842736a1549a772939b1
Brice Meyer <hi.meyerb@gmail.com>
2023-02-24 22:56:51 +0100

    Sign In View

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
+import SignIn from './pages/signIn/SignIn';
 import './assets/css/style.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
       </Routes>
       <Footer />
     </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom"
 function Header() {
     return (
         <nav className="main-nav">
-            <Link to="/">
-                <img src={ArgentBankIcon} alt='Logo Argent Bank' className='main-nav-logo'></img>
+            <Link to="/" className="main-nav-logo">
+                <img src={ArgentBankIcon} alt='Logo Argent Bank' className='main-nav-logo-image'></img>
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
 
             <div>
-                <Link to="#" className="main-nav-item">
+                <Link to="/sign-in" className="main-nav-item">
                     <i className="fa fa-user-circle icon-sign"></i>
                     Sign In 
                 </Link>
