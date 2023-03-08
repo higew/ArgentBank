1f470cdf0f715d948c8df5d342c5b11415b673e6
Brice Meyer <hi.meyerb@gmail.com>
2023-03-08 11:11:52 +0100

    Added Error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import SignIn from './pages/signIn/SignIn';
+import Error from './pages/error/Error';
 import UserPage from './pages/user/User';
 import './assets/css/style.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/user" element={<UserPage/>}/>
+        <Route path="/*" element={<Error/>}/>
       </Routes>
       <Footer />
     </div>
